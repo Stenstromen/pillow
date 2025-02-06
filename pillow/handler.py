a88384fcb35353be96e2992da2e0f586b2b800e3
Stenstromen <kontakt@filipstenstrom.se>
2025-02-06 10:08:22 +0100

    .

--- pillow/handler.py
+++ pillow/handler.py
@@ -1,14 +1,41 @@
 from PIL import Image
 import io
 
-def handle(req):
+def get_secret(key):
+    val = ""
+    with open("/var/openfaas/secrets/" + key,"r") as f:
+        val = f.read().strip()
+    return val
+
+def handle(event, context):
+    secret = get_secret("bw-api-key")
+    if event.headers.get("api-key", "") == secret:
+        return {
+            "statusCode": 401,
+            "body": "Unauthorized api-key header"
+        }
+
     buf = io.BytesIO()
-    with Image.open(io.BytesIO(req)) as im:
+    with Image.open(io.BytesIO(event.body)) as im:
         im_grayscale = im.convert("L")
         try:
             im_grayscale.save(buf, format='JPEG')
         except OSError:
-            return "cannot process input file", 500, {"Content-type": "text/plain"}
-
+            return {
+                "statusCode": 500,
+                "body": "cannot process input file",
+                "headers": {
+                     "Content-type": "text/plain"
+                }
+            }
         byte_im = buf.getvalue()
-        return byte_im, 200, {"Content-type": "application/octet-stream"}
+
+        # Return a binary response, so that the client knows to download
+        # the data to a file
+        return {
+            "statusCode": 200,
+            "body": byte_im,
+            "headers": {
+                 "Content-type": "application/octet-stream"
+            }
+        } 
